0925ba1ac2b02757e4773efc507927e5ce9c9e2b
domager73 <112970685+domager73@users.noreply.github.com>
2023-07-13 10:41:27 +0300

    refactor first code

--- lib/feature/welcome/ui/welcom_screen.dart
+++ lib/feature/welcome/ui/welcom_screen.dart
@@ -6,8 +6,20 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('Assets/images/Background.png'),
+              fit: BoxFit.cover,
+            )
+        ),
+        child: Column(
+          children: [
+
+          ],
+        ),
       ),
     );
   }
